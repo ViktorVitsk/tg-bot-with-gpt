9b9b8ead7b6ed3291055519f9d2989421ba400f4
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-10-26 20:52:54 +0300

    feat: add gpt about voice

--- src/commands/gpt.command.ts
+++ src/commands/gpt.command.ts
@@ -3,15 +3,32 @@ import { Command } from './command.class';
 import { IBotContext } from '../context/context.interface';
 import { message } from 'telegraf/filters';
 import { voiceToText } from '../utils/voice-to-text';
+import { openai } from '../openai/openai';
+import { ChatCompletionMessageParam } from 'openai/resources';
 
 export class GPTCommand extends Command {
   constructor(bot: Telegraf<IBotContext>) {
     super(bot);
   }
   handle(): void {
-    this.bot.on(message('voice'), async ctx => {
-      const text = await voiceToText(ctx);
-      ctx.reply(text);
+    this.bot.command('gpt', async ctx => {
+      ctx.reply('Шо?');
+      this.bot.on(message('voice'), async ctx => {
+        const text = await voiceToText(ctx);
+        if (text.trim().length > 0) {
+          await ctx.replyWithHTML(`<b>Ваш вопрос:</b>
+					<i>${text}</i>`);
+          const messages: ChatCompletionMessageParam[] = [
+            { content: text, role: 'user' },
+          ];
+          const response = await openai.chat(messages);
+          if (typeof response?.content === 'string') {
+            await ctx.reply(response?.content);
+          }
+        } else {
+          ctx.reply('Шото дядя, ты хуйню сказал');
+        }
+      });
     });
   }
 }
--- src/openai/openai.ts
+++ src/openai/openai.ts
@@ -2,6 +2,7 @@ import OpenAI from 'openai';
 import { IConfigService } from '../config/config.interface';
 import { ConfigService } from '../config/config.service';
 import { createReadStream } from 'fs';
+import { ChatCompletionMessageParam } from 'openai/resources';
 
 class GPT {
   openai: OpenAI;
@@ -9,9 +10,21 @@ class GPT {
     this.openai = new OpenAI({ apiKey: this.configService.get('OPEN_AI') });
   }
 
-  chat() {}
+  async chat(messages: ChatCompletionMessageParam[]) {
+    try {
+      const response = await this.openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages,
+      });
+      return response.choices[0].message;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(`Error while chat answer ${error.message}`);
+      }
+    }
+  }
 
-  async transcription(text: string) {
+  async transcription(text: string): Promise<string> {
     try {
       const response = await this.openai.audio.transcriptions.create({
         file: createReadStream(text),
